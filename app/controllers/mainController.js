0cd453efa7483b57b93ff639603a6535805f03a8
Tim Harshman <goteamtim+git@gmail.com>
2017-05-06 01:48:26 -0700

    Function to build urls

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,8 +1,19 @@
-app.controller("mainCtrl", function($scope, $http) {
+app.controller("mainCtrl", function ($scope, $http) {
     $scope.getPhoto = function () {
         $http.get('/getPhoto')
-        .then(function(response){
+            .then(function (response) {
 
-        });
-    } 
+            });
+    }
+
+    function buildFlickrUrl(farmId,serverId,id,secret) {
+        return 'https://farm' + farmId + '}.staticflickr.com/' + serverId + '}/' + 'id' + '_' + secret +'}.jpg';
+        /**
+         * https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+	or
+https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+	or
+https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
+         */
+    }
 });
