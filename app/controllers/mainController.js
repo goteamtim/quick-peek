e7913b2df00f5acb8e649a6d823efdf32063d1cc
Tim Harshman <goteamtim+git@gmail.com>
2017-05-17 00:39:39 -0700

    Add timer functionality

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,7 @@
 app.controller("mainCtrl", function ($scope, $http) {
     $scope.imageUrl;
     $scope.photosArray = [];
+    $scope.currentCountdownTimeElement = document.getElementById("currentCountdownTime");
     $scope.getPhotos = function () {
         $http.get('/getPhotos')
             .then(function (response) {
@@ -25,8 +26,21 @@ app.controller("mainCtrl", function ($scope, $http) {
                 var rand = Math.floor(Math.random()*photosArray.length);
                 var url = buildImageUrl(photosArray[rand].farm,photosArray[rand].server,photosArray[rand].id,photosArray[rand].secret);
                 displayImage(url);
+                countdownTimer();
             });
     }
-    init();
 
+    function timeUntilDate(futureDate){
+        var now = Date.now();
+        return futureDate - now;
+    }
+
+    function countdownTimer(){
+        var futureDate = localStorage.getItem("futureDate");
+        $scope.currentCountdownTimeElement = timeUntilDate(futureDate);
+        setTimeout(countdownTimer,1000);
+    }
+
+    init();
+    //setTimeout(,20000)
 });
