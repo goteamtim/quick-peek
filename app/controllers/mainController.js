1bfa7df87de670ff469e7fce2ba865b6c5116157
Tim Harshman <goteamtim+git@gmail.com>
2017-05-24 21:23:02 -0700

    More settings setup

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -10,6 +10,12 @@ app.controller("mainCtrl", function ($scope, $http) {
             });
     }
 
+    $scope.displayRandomImage = function displayRandomImage() {
+        var rand = Math.floor(Math.random() * $scope.photosArray.length);
+        var imageUrl = $scope.buildImageUrl($scope.photosArray[rand].farm, $scope.photosArray[rand].server, $scope.photosArray[rand].id, $scope.photosArray[rand].secret);
+        displayImage(imageUrl);
+    }
+
     $scope.buildImageUrl = function (farmId, serverId, id, secret) {
         return 'https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg';
     }
@@ -43,6 +49,7 @@ app.controller("mainCtrl", function ($scope, $http) {
         setTimeout(countdownTimer, 1000);
     }
 
+    
     init();
     //setTimeout(,20000)
 });
--- app/controllers/setupController.js
+++ app/controllers/setupController.js
@@ -1,3 +1,6 @@
 app.controller("setupCtrl", function($scope) {
-    
+    $scope.user = {};
+    $scope.saveSettings = function(settings){
+        localStorage.setItem('quickPeekUserSettings',JSON.stringify(settings));
+    }
 });
