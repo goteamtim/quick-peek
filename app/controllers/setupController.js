4a772a57626565805a1c8d378efeaed6fec81e8e
Tim Harshman <goteamtim+git@gmail.com>
2017-06-13 09:07:47 -0700

    Load saved settings on settings page

--- app/controllers/setupController.js
+++ app/controllers/setupController.js
@@ -2,5 +2,14 @@ app.controller("setupCtrl", function($scope) {
     $scope.user = {};
     $scope.saveSettings = function(settings){
         localStorage.setItem('quickPeekUserSettings',JSON.stringify(settings));
-    }
+    };
+
+    loadSettings = function(){
+        localStorage.getItem('quickPeekUserSettings',function(value){
+            $scope.user = JSON.parse(value);
+
+        });
+    };
+
+    loadSettings();
 });
