c6fa02fdb997d77d0b31bd4fdd40a3688e9a7b88
Tim Harshman <goteamtim+git@gmail.com>
2017-07-26 12:45:25 -0700

    Gulp fixes Made gulp actually work with nodemon and browsersync.  woot.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var reload 		= browserSync.reload;
 var nodemon     = require('nodemon')
 
 gulp.task('default', ['browser-sync'], function () {
+	gulp.watch(['app','views'], reload);
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
 	browserSync.init(null, {
-		proxy: "http://localhost:5000",
+		proxy: "http://localhost:3000",
         baseDir: "./",
-        files: ["*.*"],
+        files: ['app','views'],
         port: 7000,
 		notify: true
 	});
 });
+
 gulp.task('nodemon', function (cb) {
 	
 	var started = false;
 	
 	return nodemon({
-		script: 'index.js'
+		script: 'index.js',
+		ext: 'js html css',
+		watch: ['app','views']
 	}).on('start', function () {
 		// to avoid nodemon being started multiple times
 		// thanks @matthisk
